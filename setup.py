61eab88c74c1cade95ab0b88762fa474146be33f
msettles <msettles@uidaho.edu>
2015-07-09 15:53:36 -0300

    version bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1.dev1',
+    version='1.0.1.dev2',
 
     description='Python application for experimental high-throughput sequencing',
     long_description=long_description,
