ddcc6e8f53212bc0b3567e6c70241b176a40c7fe
David Streett <davidastreett@hotmail.com>
2015-08-25 16:55:36 -0700

    Fixed issue with multiple files

--- expHTS/validate_app.py
+++ expHTS/validate_app.py
@@ -142,7 +142,7 @@ class validateApp:
 			for subdir, dir, files in os.walk(directoryTest):
 				for file in files:
 					file = os.path.abspath(os.path.join(directoryTest, file))
-					if ("_R1" in file or "_SE") and ".fastq" in file:
+					if "_R1" in file and ".fastq" in file:
 						fastqCount += 1
 						if not sampleSeq in self.sampleFiles:
 							self.sampleFiles[sampleSeq] = []
@@ -166,7 +166,6 @@ class validateApp:
 
 	#returns 2 elements in list if paired end reads or 1 element in list if it is a SE
 	def isPairedEnd(self, fileRead1):
-
 
 		file2 = fileRead1.replace("_R1", "_R2")
 		if os.path.exists(file2):
